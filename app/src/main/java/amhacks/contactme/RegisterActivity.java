6270f776f27a1c1b12fd88b2c675797f1f68abc0
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-16 23:21:59 +0530

    Added RegisterActivity UI

--- app/src/main/java/amhacks/contactme/RegisterActivity.java
+++ app/src/main/java/amhacks/contactme/RegisterActivity.java
@@ -1,8 +1,8 @@
 package amhacks.contactme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
