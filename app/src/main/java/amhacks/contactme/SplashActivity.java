954961ceffe7d51a26770947098e0cc046cb72e1
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2020-10-16 19:50:32 +0530

    Implemented LoginActivity functionality

--- app/src/main/java/amhacks/contactme/SplashActivity.java
+++ app/src/main/java/amhacks/contactme/SplashActivity.java
@@ -1,8 +1,9 @@
 package amhacks.contactme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
