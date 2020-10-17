19fae0b461501ad57b43279de1eea7d264298d7d
Rishab Mudliar <rishabmudliar@gmail.com>
2020-10-17 11:48:29 +0530

    Added SplashActivity.java

--- app/src/main/java/amhacks/contactme/SplashActivity.java
+++ app/src/main/java/amhacks/contactme/SplashActivity.java
@@ -8,18 +8,36 @@ import android.os.Bundle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressWarnings("ALL")
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
 
+                startActivity(i);
+                //invoke the SecondActivity.
 
-        startActivity(new Intent(this, MainActivity.class));
-
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
+
+
+
 }
