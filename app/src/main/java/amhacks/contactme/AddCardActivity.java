65d8c9649a428ea5d714d8c6353c5fbcd3ec6358
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-18 12:38:58 +0530

    Added AddCardActivity

---
+++ app/src/main/java/amhacks/contactme/AddCardActivity.java
@@ -0,0 +1,166 @@
+package amhacks.contactme;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.Result;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class AddCardActivity extends AppCompatActivity {
+    CodeScanner codeScanner;
+    CodeScannerView codeScannerView;
+    FirebaseAuth mAuth;
+    DatabaseReference usersRef;
+    String pID,currentUserID;
+    private static final int RC_PERMISSION = 10;
+    private boolean mPermissionGranted;
+    private int STORAGE_PERMISSION_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        codeScannerView = (CodeScannerView) findViewById(R.id.qr_scan_view);
+        codeScanner = new CodeScanner(this,codeScannerView);
+        requestPerm();
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                pID = result.getText();
+                usersRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.child(pID).exists())
+                        {
+                            Date c = Calendar.getInstance().getTime();
+                            System.out.println("Current time => " + c);
+
+                            SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+                            final String formattedDate = df.format(c);
+                            HashMap hashMap = new HashMap();
+                            hashMap.put(currentUserID,formattedDate);
+                            usersRef.child(pID).child("Requests").updateChildren(hashMap)
+                                    .addOnCompleteListener(new OnCompleteListener() {
+                                        @Override
+                                        public void onComplete(@NonNull Task task) {
+                                            if (task.isSuccessful())
+                                            {
+                                                Toast.makeText(AddCardActivity.this, "Request sent", Toast.LENGTH_SHORT).show();
+                                                Intent dashIntent = new Intent(AddCardActivity.this,DashboardActivity.class);
+                                                dashIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                startActivity(dashIntent);
+                                            }
+                                            else
+                                            {
+                                                String msg = task.getException().getMessage();
+                                                Toast.makeText(AddCardActivity.this, msg, Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        }
+                                    });
+
+                        }
+                        else
+                        {
+                            Toast.makeText(AddCardActivity.this, "Not authorized", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+        });
+
+        codeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPerm();
+            }
+        });
+
+    }
+
+    private void requestPerm() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==
+                PackageManager.PERMISSION_GRANTED)
+        {
+            codeScanner.startPreview();
+        }
+        else
+        {
+            requestPermission();
+        }
+    }
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA))
+        {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("We need your permission to access your camera")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            ActivityCompat.requestPermissions(AddCardActivity.this, new String[] {Manifest.permission.CAMERA}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mPermissionGranted = true;
+                codeScanner.startPreview();
+            } else {
+                mPermissionGranted = false;
+            }
+        }
+    }
+}
