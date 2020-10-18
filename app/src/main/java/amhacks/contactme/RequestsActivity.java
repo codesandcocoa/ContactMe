0ef8bfc3af8f6158b9cbf28597eaaa3cefec1871
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-18 19:08:49 +0530

    Added RequestsActivity and requests_view_layout.xml

---
+++ app/src/main/java/amhacks/contactme/RequestsActivity.java
@@ -0,0 +1,14 @@
+package amhacks.contactme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requests);
+    }
+}
