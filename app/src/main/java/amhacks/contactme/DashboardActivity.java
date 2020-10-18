2c3243f9caf48a8e6a06c0459783e042395b0cc9
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2020-10-18 12:17:19 +0530

    Added MyQR View Activity

--- app/src/main/java/amhacks/contactme/DashboardActivity.java
+++ app/src/main/java/amhacks/contactme/DashboardActivity.java
@@ -1,35 +1,35 @@
 package amhacks.contactme;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
+    private LinearLayout profileLayout, ScanQRLayout, walletLayout, mycardLayout,settingsLayout,requestsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        CardView addProfile = (CardView) findViewById(R.id.add_profile_cardview);
-        CardView settings = (CardView) findViewById(R.id.settings_cardview);
-        CardView profile = (CardView) findViewById(R.id.profile_cardview);
-        CardView main = (CardView) findViewById(R.id.main_cardview);
-        main.setOnClickListener(new View.OnClickListener() {
+        profileLayout = (LinearLayout) findViewById(R.id.profile_llt);
+        ScanQRLayout  = (LinearLayout) findViewById(R.id.qr_scan_llt);
+        walletLayout = (LinearLayout) findViewById(R.id.wallet_llt);
+        mycardLayout = (LinearLayout) findViewById(R.id.mycard_llt);
+        settingsLayout = (LinearLayout) findViewById(R.id.settings_llt);
+        requestsLayout = (LinearLayout) findViewById(R.id.requests_llt);
+
+        mycardLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this,MainActivity.class);
-                startActivity(intent);
+                Intent myIntent = new Intent(DashboardActivity.this, MyCardActivity.class);
+                startActivity(myIntent);
             }
         });
-        addProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this,AddProfileActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+
     }
 }
